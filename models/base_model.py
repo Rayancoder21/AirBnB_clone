c4daf875b67df86e4f32df2a53e796fe0b34536a
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-10 09:32:03 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialize a new BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+        tFormat = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                elif k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v, tFormat))
+                else:
+                    setattr(self, k, v)
 
     def __str__(self):
         """ Return the print/str representation of the BaseModel instance"""
